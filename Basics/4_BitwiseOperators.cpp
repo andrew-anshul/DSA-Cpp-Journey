b551f4948cbafad2d99619307ebaef59f17368e2
andrew-anshul <andrewanshul798@gmail.com>
2025-02-26 19:57:53 +0530

    bitwise done

--- Basics/4_BitwiseOperators.cpp
+++ Basics/4_BitwiseOperators.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+int main() {
+    unsigned int a = 5;  // Binary: 0101
+    unsigned int b = 3;  // Binary: 0011
+    unsigned int result;
+
+    // Bitwise AND
+    result = a & b;  // 0101 & 0011 = 0001 (1 in decimal)
+    cout << "a & b = " << result << endl;
+
+    // Bitwise OR
+    result = a | b;  // 0101 | 0011 = 0111 (7 in decimal)
+    cout << "a | b = " << result << endl;
+
+    // Bitwise XOR
+    result = a ^ b;  // 0101 ^ 0011 = 0110 (6 in decimal)
+    cout << "a ^ b = " << result << endl;
+
+    // Bitwise NOT
+    result = ~a;  // ~0101 = 1010 (inverts all bits, but depends on integer size)
+    cout << "~a = " << result << endl;
+
+    // Left Shift
+    result = a << 1;  // 0101 << 1 = 1010 (10 in decimal)
+    cout << "a << 1 = " << result << endl;
+
+    // Right Shift
+    result = a >> 1;  // 0101 >> 1 = 0010 (2 in decimal)
+    cout << "a >> 1 = " << result << endl;
+
     return 0;
 }
